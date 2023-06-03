a2153d758ef8a5842a5eba12e70e7050fdcbdea9
pasiveispeter <122091935+pasiveispeter@users.noreply.github.com>
2023-06-03 16:29:36 +0545

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 import { solve, insertValues, populateValues } from './solver.js'
 import { drawBoard } from './draw_board.js'
-//import { loadRandomBoard } from './load_boards.js'
+
 
 const solveButton = document.querySelector("#solve-button")
 const clearButton = document.querySelector("#clear-button")
